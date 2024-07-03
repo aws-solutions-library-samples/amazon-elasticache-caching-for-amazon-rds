b1d571400dffffa576a414fb9af373ea40071d9a
Steven Hancz <shancz@gmail.com>
2024-07-03 17:58:46 -0400

    Update setup_host.sh

--- guidance/setup_host.sh
+++ guidance/setup_host.sh
@@ -3,6 +3,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+chmod 754 setup_jupyter.sh
+
 sudo yum install gcc python3-devel -y
 python3 -m venv .venv
 source .venv/bin/activate
